2780cf2d90c8ee6b519a8c23ff81aeb4320efe42
Franklin <franklinblanco@tutanota.com>
2022-08-28 21:23:11 -0400

    Some todos

--- src/validation/player/player_validatior.rs
+++ src/validation/player/player_validatior.rs
@@ -9,16 +9,20 @@ pub fn validate_birth_date(_birth_date: &NaiveDate) -> Result<(), MessageResourc
     Ok(())
 }
 pub fn validate_country(_country: &String) -> Result<(), MessageResource> {
+    //TODO: Validate country exists?
     Ok(())
 }
 pub fn validate_city(_city: &String) -> Result<(), MessageResource> {
+    //TODO: Validate city?
     Ok(())
 }
 /// Once an id_number is verified it cannot be changed.
 pub fn validate_identification_number(_identification_number: &String) -> Result<(), MessageResource> {
+    //TODO: Validate the id?
     Ok(())
 }
 pub fn validate_bio(_bio: &String) -> Result<(), MessageResource> {
+    //TODO: Validate bio by length & weird chars
     Ok(())
 }
 pub fn validate_profile_picture_url(_profile_picture_url: &String) -> Result<(), MessageResource>{
