fdb161120a51f98e4f3ce068f7dc4a6a24d59065
Franklin <franklinblanco@tutanota.com>
2022-09-02 20:14:36 -0400

    Added first unit test

--- src/util/text_serializer.rs
+++ src/util/text_serializer.rs
@@ -4,3 +4,12 @@ pub fn parse_sport_list() -> Vec<Sport> {
     let file = include_str!("../../sport_list.txt");
     file.split(",\n").enumerate().map(|(index, value)| Sport { id: index as i32, name: String::from(value), category_id: 0}).collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn parse_sport_list_test() {
+        assert_eq!(parse_sport_list().len(), 93);
+    }
+}
